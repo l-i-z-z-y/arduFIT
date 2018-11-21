d63cb7933ab580f0a294f3e81f0c921407c38c1e
Unknown <lustiger.luke@gmail.com>
2018-11-21 23:00:43 +0100

    absolute acc

--- ArduFit/ArduFit.ino
+++ ArduFit/ArduFit.ino
@@ -1,28 +1,35 @@
 #include <Arduino.h>
 #include <U8x8lib.h>
-
+#include <Math.h>
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
 #endif
+
 // CONSTANTS
 U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // OLEDs without Reset of the Display
 // pin config
-const int xpin = A2;                  // x-axis of the accelerometer
-const int ypin = A1;                  // y-axis
-const int zpin = A0;                  // z-axis (only on 3-axis models)
-// constants
+const int XPIN = A2;                  // x-axis of the accelerometer
+const int YPIN = A1;                  // y-axis
+const int ZPIN = A0;                  // z-axis (only on 3-axis models)
+// voltage constants
 const float zVoltageOffset = 1.65;
 const float accMax = 5;
+// absolute value of the acceleromation
+float acc;
 // variables
 int sitUps = 0;
 int pushUps = 0;
 int squads = 0;
-
+//
 int xValue = 0;
 int yValue = 0;
 int zValue = 0;
 
 float zVel = 0;
+
+float lastStateChangeTime = millis();
+const float stateChangeDifference = 10000; // 10 seconds
+
 
 void updateStateScreen(){
   u8x8.setFont(u8x8_font_chroma48medium8_r);
@@ -37,43 +44,44 @@ void updateStateScreen(){
 void setup() {
   u8x8.begin();
   u8x8.setPowerSave(0);
-  // initialize the serial communications:
   Serial.begin(9600);
   updateStateScreen();
   updateStatesOnScreen();
 }
 
 void loop() {
-// ToDo:
-// function isPushUp();
-// function isSquad();
 
   readValues();
-   
+
   if(isSitUp())
   {
     sitUps = sitUps +1;
   }
-  if (isSquad)
+  if (isSquad())
   {
     squads = squads +1;
-
   }
   if (isPushUp())
   {
     pushUps = pushUps +1;
   }
-  
+
   updateStatesOnScreen();
-  serialPrintGValues();
-  delay(100);
+  //serialPrintGValues();
+  delay(1);
 }
 
 void readValues()
 {
-  xValue = analogRead(xpin);
-  yValue = analogRead(ypin);
-  zValue = analogRead(zpin);
+  xValue = analogRead(XPIN);
+  yValue = analogRead(YPIN);
+  zValue = analogRead(ZPIN);
+
+  // calculate absolute Acceleraton
+  acc = sqrt(square(xValue) + square(yValue));
+  acc = sqrt(square(acc) + square(zValue));
+  Serial.println(acc);
+
 }
 
 void updateStatesOnScreen()
@@ -91,27 +99,32 @@ boolean isSitUp()
 {
   if(calculateGforce(zValue) > (float)0.8)
   {
+    lastStateChangeTime = millis();
     return true;
   }
-  else return false;
+  return false;
 }
 
 boolean isSquad()
 {
   if(calculateGforce(yValue) > (float)0.9)
   {
-    return true;
+    // only change state if standing up for some time
+    if (millis() - lastStateChangeTime > stateChangeDifference )
+    {
+      return true;
+    }
   }
-  else return false;
+  return false;
 }
 
 boolean isPushUp()
 {
-  if(calculateGforce(xValue) < (float)0.8)
+  if(calculateGforce(zValue) < (float)(-0.6))
   {
     return true;
   }
-  else return false;
+  return false;
 }
 
 void serialPrintGValues()
